b2a8b79ea808b204ceba2941fd1c7191e5b4373e
wmalcolm94 <wmalcolm94@outlook.com>
2016-11-12 10:32:20 -0500

    added lecture02

--- module02/lecture01/app.js
+++ module02/lecture01/app.js
@@ -2,4 +2,35 @@
 {
 'use strict';
 
+angular.module('MsgApp', [])
+.controller('MsgController', MsgController);
+
+MsgController.$inject = ['$scope', '$filter'];
+function MsgController($scope, $filter) 
+{
+    $scope.name = "William";
+    $scope.stateOfBeing = "taco";
+    $scope.foodCost = .45;
+    
+    $scope.sayMessage = function()
+    {
+        var message = "William likes to eat all of the food.";
+        var output = $filter('uppercase')(message);
+        return output;
+    };
+
+    $scope.feedMe = function()
+    {
+        if ($scope.stateOfBeing == "taco")
+        {
+            $scope.stateOfBeing = "burger";
+        }
+        else
+        {
+            $scope.stateOfBeing = "taco";
+        }
+        
+    }
+}
+
 })();
